65f76a86e9a495d677db2a07c92e06c391a0e85d
Luiz "Hik" Alves <luiz.alves@origin-games.com>
2025-10-04 22:45:25 -0300

    chore: missing commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,7 @@ import "./App.css";
 import Aladin from "./components/Aladin";
 import EncontrarNorte from "./components/scenes/EncontrarNorte";
 import Scene from "./components/Scene";
-import type { SceneNode, DialogNode } from "./components/SceneNode";
-
-const medievalNodes: SceneNode[] = [
-  { type: "dialog", text: "Welcome to the adventure!" } as DialogNode,
-  { type: "dialog", text: "Be careful, danger is everywhere." } as DialogNode,
-  {
-    type: "dialog",
-    text: "You made it to the first checkpoint!",
-  } as DialogNode,
-];
+import { medievalNodes } from "./NodesConfig";
 
 function App() {
   const [aladinInstance, setAladinInstance] = useState(null);
