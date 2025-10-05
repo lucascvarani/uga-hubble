a740485c400bbcff28c0afc598e606b1416a9dfe
Lucas Varani <42304402+lucascvarani@users.noreply.github.com>
2025-10-05 13:34:48 -0300

    Fix eye

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,13 @@ import { ugaNodes } from './components/scenes/S1Uga'
 
 function App() {
   const [aladinInstance, setAladinInstance] = useState(null)
-  const [sceneNumber, setSceneNumber] = useState<number>(0)
+  const [sceneNumber, setSceneNumber] = useState<number>(1)
   const [showSoundWarning, setShowSoundWarning] = useState(true)
   const [showIntro, setShowIntro] = useState(true)
 
   // Show sound warning first
   if (showSoundWarning) {
     return <SoundWarning onProceed={() => setShowSoundWarning(false)} />
-  }
-
-  // Then show intro
-  if (showIntro) {
-    return <FireMenu onStart={() => setShowIntro(false)} />
   }
 
   return (
@@ -34,60 +29,67 @@ function App() {
       }}
     >
       {/* Full screen Aladin */}
-      <div style={{ width: '100%', height: '100%' }}>
+      <div
+        id="aladin-app-container invisible"
+        style={{ width: '100%', height: '100%' }}
+      >
         <Aladin setAladinInstance={setAladinInstance} />
       </div>
 
-      {/* Scene overlay - Full Screen */}
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          pointerEvents: 'none',
-          zIndex: 10,
-        }}
-      >
-        <div style={{ pointerEvents: 'auto' }}>
-          {(() => {
-            switch (sceneNumber) {
-              case 0:
-                return (
-                  <Scene
-                    aladinInstance={aladinInstance}
-                    nodes={ugaNodes}
-                    onSceneEnd={() =>
-                      setSceneNumber((previous) => previous + 1)
-                    }
-                  />
-                )
-              case 1:
-                return (
-                  <Scene
-                    aladinInstance={aladinInstance}
-                    nodes={medievalNodes}
-                    onSceneEnd={() =>
-                      setSceneNumber((previous) => previous + 1)
-                    }
-                  />
-                )
-              case 9:
-                return (
-                  <Galaxies
-                    aladinInstance={aladinInstance}
-                    onComplete={() =>
-                      setSceneNumber((previous) => previous + 1)
-                    }
-                  />
-                )
-              default:
-                return null
-            }
-          })()}
+      {showIntro ? (
+        <FireMenu onStart={() => setShowIntro(false)} />
+      ) : (
+        /* Scene overlay - Full Screen */
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            pointerEvents: 'none',
+            zIndex: 10,
+          }}
+        >
+          <div style={{ pointerEvents: 'auto' }}>
+            {(() => {
+              switch (sceneNumber) {
+                case 0:
+                  return (
+                    <Scene
+                      aladinInstance={aladinInstance}
+                      nodes={ugaNodes}
+                      onSceneEnd={() =>
+                        setSceneNumber((previous) => previous + 1)
+                      }
+                    />
+                  )
+                case 1:
+                  return (
+                    <Scene
+                      aladinInstance={aladinInstance}
+                      nodes={medievalNodes}
+                      onSceneEnd={() =>
+                        setSceneNumber((previous) => previous + 1)
+                      }
+                    />
+                  )
+                case 9:
+                  return (
+                    <Galaxies
+                      aladinInstance={aladinInstance}
+                      onComplete={() =>
+                        setSceneNumber((previous) => previous + 1)
+                      }
+                    />
+                  )
+                default:
+                  return null
+              }
+            })()}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
