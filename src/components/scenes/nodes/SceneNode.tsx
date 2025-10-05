8da0ec119a93591beea393f0e7da1e0b123d0d70
Lucas Varani <42304402+lucascvarani@users.noreply.github.com>
2025-10-05 11:27:13 -0300

    Prettir SneceNode

--- src/components/scenes/nodes/SceneNode.tsx
+++ src/components/scenes/nodes/SceneNode.tsx
@@ -1,37 +1,42 @@
-export type SceneNodeType = "dialog" | "find" | "constellation"; // add more types later
+export type SceneNodeType = 'dialog' | 'find' | 'constellation' | 'eye' // add more types later
 
 export interface SceneNode {
-  type: SceneNodeType;
+  type: SceneNodeType
 }
 
 export interface DialogNode extends SceneNode {
-  type: "dialog";
-  text: string;
+  type: 'dialog'
+  text: string
+}
+
+export interface EyeNode extends SceneNode {
+  type: 'eye'
+  coords: { ra: number; dec: number }
 }
 
 export interface FindPosNode extends SceneNode {
-  type: "find" | "constellation",
-  title: string,
-  description: string,
-  startingCoords?: { ra: number; dec: number }; // starting position
-  targetCoords: { ra: number; dec: number };   // target to find
-  survey?: string; // e.g. "P/DSS2/color" or "P/2MASS/color"
-  tolerance: number;
-  fov: number;
+  type: 'find' | 'constellation'
+  title: string
+  description: string
+  startingCoords?: { ra: number; dec: number } // starting position
+  targetCoords: { ra: number; dec: number } // target to find
+  survey?: string // e.g. "P/DSS2/color" or "P/2MASS/color"
+  tolerance: number
+  fov: number
 }
 
 interface Star {
-  ra: number;
-  dec: number;
-  name?: string;
+  ra: number
+  dec: number
+  name?: string
 }
 
 export interface CompleteConstellationNode extends FindPosNode {
-  type: "constellation",
+  type: 'constellation'
   constellation: {
-    name: string;
-    stars: Star[];
+    name: string
+    stars: Star[]
     // The lines of the constellation, each a list of [ra, dec] pairs forming a polyline
-    lines: number[][][]; 
-  };
+    lines: number[][][]
+  }
 }
